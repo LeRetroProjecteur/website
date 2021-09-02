1ea5af177474c6c91f08a59f9c0ac71f0fead793
tanouch <ugo.tanielian@outlook.fr>
2021-09-02 10:02:23 +0200

    Release 2.3
    Fixing the table rendering problem and the buttons appearances..

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 'use strict'
 
-const switcher = document.querySelector('.btn');
-
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme')
-
-    var className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark Mode!";
-    }
-    else {
-        this.textContent = "Light Mode!";
-    }
-
-    console.log('current class name: ' + className);
-
-});
+// const switcher = document.querySelector('.btn');
+//
+// switcher.addEventListener('click', function() {
+//     document.body.classList.toggle('dark-theme')
+//
+//     var className = document.body.className;
+//     if(className == "light-theme") {
+//         this.textContent = "Dark Mode!";
+//     }
+//     else {
+//         this.textContent = "Light Mode!";
+//     }
+//
+//     console.log('current class name: ' + className);
+//
+// });
