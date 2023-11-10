4dc7bad3836a9c4b1d4a3d9e43a8d98e06f73aeb
Jonathan Roitgrund <jroitgrund@gmail.com>
2023-11-10 19:00:07 +0200

    Fix cache

--- src/lib/movies.ts
+++ src/lib/movies.ts
@@ -41,23 +41,31 @@ export const getMovies = unstable_cache(
   { revalidate: hoursToSeconds(1) },
 );
 
-export const getReviewedMovies = unstable_cache(async () => {
-  const { db } = getFirebase();
-  const q = doc(db, "website-extra-docs", "all-reviews");
-  let querySnapshot = await getDoc(q);
-  return checkNotNull(querySnapshot.data()).elements as Review[];
-});
+export const getReviewedMovies = unstable_cache(
+  async () => {
+    const { db } = getFirebase();
+    const q = doc(db, "website-extra-docs", "all-reviews");
+    let querySnapshot = await getDoc(q);
+    return checkNotNull(querySnapshot.data()).elements as Review[];
+  },
+  ["reviewed-movies"],
+  { revalidate: hoursToSeconds(1) },
+);
 
-export const getMovie = unstable_cache(async (id: string) => {
-  const { db } = getFirebase();
-  const q = query(
-    collection(db, "website-by-movie-screenings"),
-    where("id", "==", id),
-  );
-  const querySnapshot = await getDocs(q);
-  let data_aux: MovieDetail[] = [];
-  querySnapshot.forEach((doc) => {
-    data_aux.push(doc.data() as MovieDetail);
-  });
-  return data_aux[0];
-});
+export const getMovie = unstable_cache(
+  async (id: string) => {
+    const { db } = getFirebase();
+    const q = query(
+      collection(db, "website-by-movie-screenings"),
+      where("id", "==", id),
+    );
+    const querySnapshot = await getDocs(q);
+    let data_aux: MovieDetail[] = [];
+    querySnapshot.forEach((doc) => {
+      data_aux.push(doc.data() as MovieDetail);
+    });
+    return data_aux[0];
+  },
+  ["single-movie"],
+  { revalidate: hoursToSeconds(1) },
+);
