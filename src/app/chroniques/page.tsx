e877ae5d7b69396945dc8b4ae36192ed54e2f62e
Jonathan Roitgrund <jroitgrund@gmail.com>
2023-11-13 11:58:09 +0200

    Chroniques - generate list from variable

--- src/app/chroniques/page.tsx
+++ src/app/chroniques/page.tsx
@@ -6,56 +6,50 @@ import lumiereIcon from "./festival-lumiere-2023/img/icon.png";
 import top2022 from "./top-2022/img/icon.png";
 
 export const metadata: Metadata = {
-  title: "Chroniques | Le Rétro Projecteur - Cinéma de patrimoine à Paris",
+  title: "Chroniques | Le Rétro Projecteur — Cinéma de patrimoine à Paris",
 };
+
+const chroniques = [
+  {
+    titre: "Retour sur le Festival Lumière 2023",
+    slug: "festival-lumiere-2023",
+    image: lumiereIcon,
+    date: "31 octobre, 2023",
+  },
+  {
+    titre: "Rétrospective 2022 — Nos meilleures séances patrimoine",
+    slug: "top-2022",
+    image: top2022,
+    date: "2 janvier, 2023",
+  },
+];
 
 export default function Chroniques() {
   return (
     <>
       <h2>Chroniques</h2>
       <div className="post-grid">
-        <div className="post">
-          <Link href="chroniques/festival-lumiere-2023/">
-            <Image
-              width={274}
-              height={150}
-              className="post-icon"
-              src={lumiereIcon}
-              alt="Festival Lumière"
-            />
-          </Link>
-          <p>
-            <strong>
-              <Link href="chroniques/festival-lumiere-2023/">
-                Retour sur le Festival Lumière 2023
-              </Link>
-            </strong>
-          </p>
-          <p style={{ textAlign: "right" }}>
-            <i>31 octobre, 2023</i>
-          </p>
-        </div>
-        <div className="post">
-          <Link href="chroniques/top-2022/">
-            <Image
-              width={274}
-              height={150}
-              className="post-icon"
-              src={top2022}
-              alt="Top 2022"
-            />
-          </Link>
-          <p>
-            <strong>
-              <Link href="chroniques/top-2022/">
-                Rétrospective 2022 – Nos meilleures séances patrimoine
-              </Link>
-            </strong>
-          </p>
-          <p style={{ textAlign: "right" }}>
-            <i>2 janvier, 2023</i>
-          </p>
-        </div>
+        {chroniques.map(({ titre, slug, image, date }) => (
+          <div className="post" key={slug}>
+            <Link href={`chroniques/${slug}`}>
+              <Image
+                width={274}
+                height={150}
+                className="post-icon"
+                src={image}
+                alt={titre}
+              />
+            </Link>
+            <p>
+              <strong>
+                <Link href={`chroniques/${slug}`}>{titre}</Link>
+              </strong>
+            </p>
+            <p style={{ textAlign: "right" }}>
+              <i>{date}</i>
+            </p>
+          </div>
+        ))}
       </div>
     </>
   );
