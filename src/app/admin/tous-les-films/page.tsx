a36d9b6276cae49c5aa7b86e9e6342b2cea9648a
tanouch <ugo.tanielian@gleamer.ai>
2023-11-20 00:54:46 +0100

    removing netlify.toml - correcting lag for the week generation. - adding link for all_movies_page.

--- src/app/admin/tous-les-films/page.tsx
+++ src/app/admin/tous-les-films/page.tsx
@@ -1,11 +1,19 @@
 import { Suspense } from "react";
 
 import Calendrier from "@/components/calendrier";
+import {format, isSameMonth} from "date-fns";
+import {fr} from "date-fns/locale";
+import {FilterableMovies} from "@/app/admin/semaine/semaine";
 
 export default function TousLesFilms() {
   return (
-    <Suspense fallback={<></>}>
-      <Calendrier allMovies={true} />
-    </Suspense>
+  <>
+      <h2>
+          Bienvenue Frère Lumar !
+      </h2>
+        <Suspense fallback={<></>}>
+          <Calendrier allMovies={true} />
+        </Suspense>
+  </>
   );
 }
