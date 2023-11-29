8c3ff436244986d325764d0e47cda39905027e00
Jonathan Roitgrund <jroitgrund@gmail.com>
2023-11-29 19:41:11 +0200

    restore cols & fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,17 +7,17 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    // extend: {
-    //   colors: {
-    //     ["retro-gray"]: "#6A6A6D",
-    //     ["retro-black"]: "#030303",
-    //     ["retro-green"]: "#E2FF46",
-    //   },
-    //   fontFamily: {
-    //     degular: ["var(--font-degular)"],
-    //     suisse: ["var(--font-suisse)"],
-    //   },
-    // },
+    extend: {
+      colors: {
+        ["retro-gray"]: "#6A6A6D",
+        ["retro-black"]: "#030303",
+        ["retro-green"]: "#E2FF46",
+      },
+      fontFamily: {
+        degular: ["var(--font-degular)"],
+        suisse: ["var(--font-suisse)"],
+      },
+    },
   },
   plugins: [],
 };
