75b7ebb4cda3a88ba63aac81abc9ac887da0524b
Jonathan Roitgrund <jroitgrund@gmail.com>
2023-12-04 19:11:07 +0200

    Scroll page headers

--- src/app/(calendrier)/calendrier.tsx
+++ src/app/(calendrier)/calendrier.tsx
@@ -5,6 +5,7 @@ import { useCallback, useState } from "react";
 import DateSelector from "@/app/(calendrier)/date-selector";
 import QuartierSelector from "@/app/(calendrier)/quartier-selector";
 import TimeSlider from "@/app/(calendrier)/time-slider";
+import FixedHeader from "@/components/layout/fixed-header";
 import PageHeader from "@/components/layout/page-header";
 import { Movie } from "@/lib/types";
 
@@ -26,14 +27,14 @@ export default function Calendrier({
 
   return (
     <div className="flex grow flex-col">
-      <div className="flex flex-col">
+      <FixedHeader className="flex flex-col pb-4">
         <div className="flex lg:pb-4">
           <PageHeader text="calendrier" />
         </div>
         <div className="flex border-b py-3 lg:border-0 lg:py-0">
           <DateSelector />
         </div>
-      </div>
+      </FixedHeader>
       <div className="flex flex-col lg:pl-4">
         <div className="flex pb-6 lg:pb-8">
           <TimeSlider />
---
+++ src/components/layout/fixed-header.tsx
@@ -0,0 +1,21 @@
+import clsx from "clsx";
+import { ReactNode } from "react";
+
+export default function FixedHeader({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <div
+      className={clsx(
+        className,
+        "z-20 flex flex-col bg-white lg:sticky lg:top-0 lg:pb-4 lg:pt-4",
+      )}
+    >
+      {children}
+    </div>
+  );
+}
