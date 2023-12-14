649c125cdf3bf95201a8ccfcaad4bdbb333a3e60
tanouch <ugo.tanielian@gleamer.ai>
2023-12-14 20:07:39 +0100

    deleting metadata

--- src/app/top2023/page.tsx
+++ src/app/top2023/page.tsx
@@ -5,6 +5,15 @@ import {Metadata} from "next";
 
 export const metadata: Metadata = {
     title: "Sondage Top 2023 | Le Rétro Projecteur – Cinéma de patrimoine à Paris",
+    twitter: {
+    card: "summary_large_image",
+    title: "Generate Dynamic Open Graph and Twitter Images in Next.js",
+    description:
+      "A guide on how to optimize SEO with static and dynamic metatags using Next.js 13's new Metadata API.",
+    images: [
+      "https://cruip-tutorials-next.vercel.app/api/og?title=Generate Dynamic Open Graph and Twitter Images in Next.js",
+    ],
+  },
 };
 
 export default function SemaineAuCinemaPage() {
