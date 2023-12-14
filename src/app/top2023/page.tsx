1dfc033c9341b9ba2123fb3cef636f960b74b79d
tanouch <ugo.tanielian@gleamer.ai>
2023-12-14 21:55:16 +0100

    metadata

--- src/app/top2023/page.tsx
+++ src/app/top2023/page.tsx
@@ -5,23 +5,6 @@ import {Metadata} from "next";
 
 export const metadata: Metadata = {
     title: "Sondage Top 2023 | Le Rétro Projecteur – Cinéma de patrimoine à Paris",
-    openGraph: {
-      url: `https://firebasestorage.googleapis.com/v0/b/website-cine.appspot.com/o/images%2Ftop2023.png?alt=media`,
-      images: [
-        {
-          url: `https://firebasestorage.googleapis.com/v0/b/website-cine.appspot.com/o/images%2Ftop2023.png?alt=media`,
-        },
-      ],
-    },
-    twitter: {
-        card: "summary_large_image",
-        title: "Sondage Top 2023 | Le Rétro Projecteur – Cinéma de patrimoine à Paris",
-        description:
-          "Sondage Top 2023 | Le Rétro Projecteur – Cinéma de patrimoine à Paris.",
-        images: [
-          `https://firebasestorage.googleapis.com/v0/b/website-cine.appspot.com/o/images%2Ftop2023.png?alt=media`,
-        ],
-  },
 };
 
 export default function SemaineAuCinemaPage() {
