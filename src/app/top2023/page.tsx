8a8d51b457eb78341ca388229bd5209d9cc338f3
tanouch <ugo.tanielian@gleamer.ai>
2023-12-14 20:48:07 +0100

    cleaning metadata.

--- src/app/top2023/page.tsx
+++ src/app/top2023/page.tsx
@@ -7,9 +7,9 @@ export const metadata: Metadata = {
     title: "Sondage Top 2023 | Le Rétro Projecteur – Cinéma de patrimoine à Paris",
     twitter: {
         card: "summary_large_image",
-        title: "Generate Dynamic Open Graph and Twitter Images in Next.js",
+        title: "Sondage Top 2023 | Le Rétro Projecteur – Cinéma de patrimoine à Paris",
         description:
-          "A guide on how to optimize SEO with static and dynamic metatags using Next.js 13's new Metadata API.",
+          "Sondage Top 2023 | Le Rétro Projecteur – Cinéma de patrimoine à Paris.",
         images: [
           `https://firebasestorage.googleapis.com/v0/b/website-cine.appspot.com/o/images%2Ftop2023.png?alt=media`,
         ],
