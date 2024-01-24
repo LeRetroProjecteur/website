ab43192543fd214704a6656ada4c920760ef9272
Nico Guetta-Jeanrenaud <nicolas.jeanrenaud@gmail.com>
2024-01-24 17:48:00 +0100

    reviews: removing closeMenu

--- src/components/layout/top-bar.tsx
+++ src/components/layout/top-bar.tsx
@@ -2,27 +2,20 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useCallback } from "react";
 
-import { useCalendrierStore } from "@/lib/calendrier-store";
-import { closeMenu, openMenu } from "@/lib/menu-store";
+import { openMenu } from "@/lib/menu-store";
 
 import logoAnime from "../../assets/logo-anime.gif";
 import BurgerIcon from "../menu/burger-icon";
 
 export default function TopBar() {
-  const onClickLogo = useCallback(() => {
-    useCalendrierStore.getState().reset();
-    closeMenu();
-  }, []);
-
   return (
     <div className="relative flex grow items-center">
       <div className="absolute cursor-pointer" onClick={openMenu}>
         <BurgerIcon />
       </div>
       <div className="flex grow justify-center">
-        <Link href="/" onClick={onClickLogo}>
+        <Link href="/">
           <Image src={logoAnime} alt="logo" className="w-200px" />
         </Link>
       </div>
