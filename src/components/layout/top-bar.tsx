f88ff8b7284640c3af604cb3e1ea85713e00416e
Nico Guetta-Jeanrenaud <nicolas.jeanrenaud@gmail.com>
2024-01-24 11:58:06 +0100

    adding link to mobile header

--- src/components/layout/top-bar.tsx
+++ src/components/layout/top-bar.tsx
@@ -1,20 +1,30 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
+import { useCallback } from "react";
 
-import { openMenu } from "@/lib/menu-store";
+import { useCalendrierStore } from "@/lib/calendrier-store";
+import { closeMenu, openMenu } from "@/lib/menu-store";
 
 import logoAnime from "../../assets/logo-anime.gif";
 import BurgerIcon from "../menu/burger-icon";
 
 export default function TopBar() {
+  const onClickLogo = useCallback(() => {
+    useCalendrierStore.getState().reset();
+    closeMenu();
+  }, []);
+
   return (
     <div className="relative flex grow items-center">
       <div className="absolute cursor-pointer" onClick={openMenu}>
         <BurgerIcon />
       </div>
       <div className="flex grow justify-center">
-        <Image src={logoAnime} alt="logo" className="w-200px" />
+        <Link href="/" onClick={onClickLogo}>
+          <Image src={logoAnime} alt="logo" className="w-200px" />
+        </Link>
       </div>
     </div>
   );
