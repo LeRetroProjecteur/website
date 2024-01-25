1eaa22231f19562b4fc16960d451aca525ccf38e
Nico Guetta-Jeanrenaud <nicolas.jeanrenaud@gmail.com>
2024-01-25 22:36:52 +0100

    reviews

--- src/components/layout/top-bar.tsx
+++ src/components/layout/top-bar.tsx
@@ -2,20 +2,26 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { useCallback } from "react";
 
+import { useCalendrierStore } from "@/lib/calendrier-store";
 import { openMenu } from "@/lib/menu-store";
 
 import logoAnime from "../../assets/logo-anime.gif";
 import BurgerIcon from "../menu/burger-icon";
 
 export default function TopBar() {
+  const onClickLogo = useCallback(() => {
+    useCalendrierStore.getState().reset();
+  }, []);
+
   return (
     <div className="relative flex grow items-center">
       <div className="absolute cursor-pointer" onClick={openMenu}>
         <BurgerIcon />
       </div>
       <div className="flex grow justify-center">
-        <Link href="/">
+        <Link href="/" onClick={onClickLogo}>
           <Image src={logoAnime} alt="logo" className="w-200px" />
         </Link>
       </div>
