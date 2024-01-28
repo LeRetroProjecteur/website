0d0a139d2e0718c3119ac89df95a7c4245d493f5
Nico Guetta-Jeanrenaud <nicolas.jeanrenaud@gmail.com>
2024-01-28 11:21:28 +0100

    linting

--- src/app/film/[movieId]/film.tsx
+++ src/app/film/[movieId]/film.tsx
@@ -99,7 +99,7 @@ function MovieInfo({ movie }: { movie: MovieDetail }) {
         </div>
       )}
 
-      <div className="flex pt-15px lg:py-20px lg:border-y">
+      <div className="flex pt-15px lg:border-y lg:py-20px">
         <MetaCopy>
           <div>Titre original&nbsp;: {movie.original_title}</div>
           {movie.duration == null ? (
