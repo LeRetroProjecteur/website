0c8a84ff5b16df336e8c8783f22c2fbfd0001a05
Nico Guetta-Jeanrenaud <nicolas.jeanrenaud@gmail.com>
2024-02-06 16:01:18 +0100

    NOT WORKING: factorizing some of the A Propos page

--- src/app/a-propos/page.tsx
+++ src/app/a-propos/page.tsx
@@ -59,6 +59,72 @@ function Project() {
   );
 }
 
+const teamMembers = [
+  {
+    name: "Nicolas Guetta-Jeanrenaud",
+    role: "Rédacteur",
+    socials: [
+      { name: "Twitter", link: "https://twitter.com/nicogj_" },
+      { name: "Letterboxd", link: "https://letterboxd.com/nicogj/" },
+    ],
+    films: [
+      { name: "Metropolis", link: "/film/metropolis-1927" },
+      { name: "Ma Nuit chez Maud", link: "/film/nuit-chez-maud-1969" },
+      { name: "Printemps Tardif", link: "/film/printemps-tardif-1949" },
+      { name: "Old Joy", link: "/film/old-joy-2006" },
+    ],
+  },
+  {
+    name: "Lionel Guetta-Jeanrenaud",
+    role: "Rédacteur",
+    socials: [
+      { name: "Twitter", link: "https://twitter.com/liojeanrenaud" },
+      { name: "Letterboxd", link: "https://letterboxd.com/lioguetta/" },
+    ],
+    films: [
+      { name: "Les Parapluies de Cherbourg", link: "/film/parapluies-cherbourg-1963" },
+      { name: "Jeannette, l'enfance de Jeanne d'Arc", link: "/film/jeannette-enfance-jeanne-arc-2017" },
+      { name: "L'Atalante", link: "/film/l-atalante-1934" },
+      { name: "Marie-Antoinette", link: "/film/marie-antoinette-2006" },
+    ],
+  },
+  {
+    name: "Ugo Tanielian",
+    role: "Technologiste",
+    socials: [],
+    films: [
+      { name: "Il était une fois dans l'Ouest", link: "/film/etait-fois-ouest-1968" },
+      { name: "John McCabe", link: "/film/john-mccabe-1971" },
+      { name: "Voyage au bout de l'enfer", link: "/film/voyage-bout-enfer-1978" },
+      { name: "Nothing But a Man", link: "/film/nothing-man-1964" },
+    ],
+  },
+  {
+    name: "Claire Malot",
+    role: "Designer graphique",
+    socialsIntro: "Retrouvez mon travail sur ",
+    socials: [
+      { name: "Instagram", link: "https://www.instagram.com/clairon.malot/" },
+      { name: "mon site personnel", link: "https://clairemalot.com/" },
+    ],
+    films: [
+      { name: "L'un chante, l'autre pas", link: "/film/chante-autre-1977" },
+      { name: "Conte d'été", link: "/film/conte-d-ete-1996" },
+      { name: "First Cow", link: "/film/first-cow-2020" },
+    ],
+  },
+  {
+    name: "Jonathan Roitgrund",
+    role: "Développeur",
+    socials: [],
+    films: [
+      { name: "After Hours", link: "/film/after-hours-1985" },
+      { name: "Stranger than Paradise", link: "/film/stranger-than-paradise-1984" },
+      { name: "Ça tourne à Manhattan", link: "https://fr.wikipedia.org/wiki/%C3%87a_tourne_%C3%A0_Manhattan" },
+    ],
+  },
+];
+
 function Team() {
   return (
     <div>
@@ -66,188 +132,85 @@ function Team() {
         <SousTitre2>l&apos;équipe du rétro</SousTitre2>
       </div>
       <div>
-        <Contributeur>
-          <span className="font-semibold">Nicolas Guetta-Jeanrenaud</span>
-          <br />
-          <span className="font-semibold uppercase">Rédacteur en chef</span>
-          <br />
-          Retrouvez-moi sur{" "}
-          <a
-            className="underline"
-            href="https://letterboxd.com/nicogj/"
-            target="_blank"
-          >
-            Letterboxd
-          </a>
-          ,{" "}
-          <a
-            className="underline"
-            href="https://twitter.com/nicogj_"
-            target="_blank"
-          >
-            Twitter
-          </a>
-          <br />
-          <Image
-            className="mt-[-0.1875rem] inline-block h-21px w-auto w-auto pr-5px"
-            alt="coup de coeur"
-            src={coupDeCoeur}
-          />{" "}
-          <Film name="Metropolis" inLink="/film/metropolis-1927" />,{" "}
-          <Film name="Ma Nuit chez Maud" inLink="/film/nuit-chez-maud-1969" />,{" "}
-          <Film name="Printemps Tardif" inLink="/film/printemps-tardif-1949" />
-          , <Film name="Old Joy" inLink="/film/old-joy-2006" />
-        </Contributeur>
-        <Contributeur>
-          <span className="font-semibold">Lionel Guetta-Jeanrenaud</span>
-          <br />
-          <span className="font-semibold uppercase">Rédacteur en chef</span>
-          <br />
-          Retrouvez-moi sur{" "}
-          <a
-            className="underline"
-            href="https://letterboxd.com/lioguetta/"
-            target="_blank"
-          >
-            Letterboxd
-          </a>
-          ,{" "}
-          <a
-            className="underline"
-            href="https://twitter.com/liojeanrenaud"
-            target="_blank"
-          >
-            Twitter
-          </a>
-          <br />
-          <Image
-            className="mt-[-0.1875rem] inline-block h-21px w-auto w-auto pr-5px"
-            alt="coup de coeur"
-            src={coupDeCoeur}
-          />{" "}
-          <Film
-            name="Les Parapluies de Cherbourg"
-            inLink="/film/parapluies-cherbourg-1963"
-          />
-          ,{" "}
-          <Film
-            name="Jeannette, l'enfance de Jeanne d'Arc"
-            inLink="/film/jeannette-enfance-jeanne-arc-2017"
-          />
-          , <Film name="L'Atalante" inLink="/film/l-atalante-1934" />,{" "}
-          <Film name="Marie-Antoinette" inLink="/film/marie-antoinette-2006" />
-        </Contributeur>
-        <Contributeur>
-          <span className="font-semibold">Ugo Tanielian</span>
-          <br />
-          <span className="font-semibold uppercase">Rédacteur en chef</span>
-          <br />
-          <Image
-            className="mt-[-0.1875rem] inline-block h-21px w-auto w-auto pr-5px"
-            alt="coup de coeur"
-            src={coupDeCoeur}
-          />{" "}
-          <Film
-            name="Il était une fois dans l'Ouest"
-            inLink="/film/etait-fois-ouest-1968"
-          />
-          , <Film name="John McCabe" inLink="/film/john-mccabe-1971" />,{" "}
-          <Film
-            name="Voyage au bout de l'enfer"
-            inLink="/film/voyage-bout-enfer-1978"
-          />
-          , <Film name="Nothing But a Man" inLink="/film/nothing-man-1964" />
-        </Contributeur>
-        <Contributeur>
-          <span className="font-semibold">Claire Malot</span>
-          <br />
-          <span className="font-semibold uppercase">designer graphique</span>
-          <br />
-          Retrouvez mon travail sur{" "}
-          <a
-            href="https://www.instagram.com/clairon.malot/"
-            className="underline"
-            target="_blank"
-          >
-            Instagram
-          </a>{" "}
-          et sur mon{" "}
-          <a
-            href="https://clairemalot.com/"
-            className="underline"
-            target="_blank"
-          >
-            site personnel
-          </a>
-          .
-          <br />
-          <Image
-            className="mt-[-0.1875rem] inline-block h-21px w-auto w-auto pr-5px"
-            alt="coup de coeur"
-            src={coupDeCoeur}
-          />{" "}
-          <Film
-            name="L'un chante, l'autre pas"
-            inLink="/film/chante-autre-1977"
-          />
-          , <Film name="Conte d'été" inLink="/film/conte-d-ete-1996" />,{" "}
-          <Film name="First Cow" inLink="/film/first-cow-2020" />
-        </Contributeur>
-        <Contributeur>
-          <span className="font-semibold">Jonathan Roitgrund</span>
-          <br />
-          <span className="font-semibold uppercase">développeur</span>
-          <br />
-          <Image
-            className="mt-[-0.1875rem] inline-block h-21px w-auto w-auto pr-5px"
-            alt="coup de coeur"
-            src={coupDeCoeur}
-          />{" "}
-          <Film name="After Hours" inLink="/film/after-hours-1985" />,{" "}
-          <Film
-            name="Stranger than Paradise"
-            inLink="/film/stranger-than-paradise-1984"
-          />
-          ,{" "}
-          <Film
-            name="Ça tourne à Manhattan"
-            outLink="https://fr.wikipedia.org/wiki/%C3%87a_tourne_%C3%A0_Manhattan"
-          />
-        </Contributeur>
+        {teamMembers.map((teamMember) => (
+          <TeamMember key={teamMember.name} member={teamMember} />
+        ))}
       </div>
-    </div>
-  );
-}
-
-function Contributeur({ children }: { children: ReactNode }) {
-  return (
-    <div className="border-b py-17px text-center">
-      <BodyCopy>{children}</BodyCopy>
     </div>
   );
 }
 
 function Film({
   name,
-  inLink,
-  outLink,
+  link,
 }: {
   name: string;
-  inLink?: string;
-  outLink?: string;
+  link: string;
 }) {
   return (
-    <i className="italic">
-      {inLink != null && (
-        <Link href={inLink} className="underline">
-          {name}
-        </Link>
-      )}
-      {outLink != null && (
-        <a target="_blank" href={outLink} className="underline">
-          {name}
-        </a>
-      )}
+    <i>
+      <Link href={link} className="underline">
+        {name}
+      </Link>
     </i>
+  );
+}
+
+function Social(
+  {name, link}: {name:string, link:string}
+) {
+  return (
+    <span>
+      <a href={link} className="underline" target="_blank">
+        {name}
+      </a>
+    </span>
+  );
+}
+
+
+function TeamMember({
+  member,
+}: {
+  member: {
+    name: string;
+    role: string;
+    socialsIntro?: string;
+    socials: ReactNode[];
+    films: ReactNode[];
+  };
+}) {
+  return (
+    <div className="border-b py-17px text-center">
+      <BodyCopy>
+        <div>{member.name}</div>
+        <div className="uppercase">{member.role}</div>
+        {member.socials.length > 0 && (
+          <div>
+            {member.socialsIntro != null
+              ? member.socialsIntro
+              : "Retrouvez moi sur "
+            }
+            {member.socials
+              .map((social) => (
+                <Social key={social.name} name={social.name} link={social.link} />
+              ))
+              .join(", ")
+            }
+            .
+          </div>
+        )}
+        <Image
+          className="mt-[-0.1875rem] inline-block h-21px w-auto w-auto pr-5px"
+          alt="coup de coeur"
+          src={coupDeCoeur}
+        />{" "}
+        {member.films
+          .map(( film ) => (
+            <Film key={film.name} name={film.name} link={film.link} />
+          ))
+          .join(", ")}
+      </BodyCopy>
+    </div>
   );
 }
