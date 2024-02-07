790f7fc7166680512b44967c23cbdf0a39a9e1c8
Nico Guetta-Jeanrenaud <nicolas.jeanrenaud@gmail.com>
2024-02-08 00:12:33 +0100

    troubleshooting mid vertical line

--- src/components/layout/two-column-page.tsx
+++ src/components/layout/two-column-page.tsx
@@ -8,14 +8,14 @@ export function TwoColumnPage({
   right: ReactNode;
 }) {
   return (
-    <div className="flex flex-col lg:pl-20px">
-      <div className="flex grow flex-col lg:flex-row">
-        <div className="flex grow flex-col lg:w-1/2 lg:border-r lg:pr-20px">
+    <div className="flex grow flex-col lg:pl-20px">
+      <div className="flex flex-col lg:flex-row">
+        <div className="flex flex-col lg:w-1/2 lg:border-r lg:pr-20px">
           {left}
         </div>
         <div className="flex flex-col lg:w-1/2 lg:pl-20px">{right}</div>
       </div>
-      <div className="min-h-100px w-1/2 lg:border-r" />
+      <div className="min-h-100px w-1/2 grow border-r" />
     </div>
   );
 }
