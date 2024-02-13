f06b1a9341a40d691967c344ac8cd26e1f463782
tanouch <ugo.tanielian@gleamer.ai>
2024-02-13 20:00:29 +0100

    Changes.

--- src/app/(calendrier)/date-selector.tsx
+++ src/app/(calendrier)/date-selector.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { DateTime } from "luxon";
 import { useCallback } from "react";
 
-// Import luxon DateTime library
 import { LeftArrow, RightArrow } from "@/components/icons/arrows";
 import { SousTitre1 } from "@/components/typography/typography";
 import { useCalendrierStore } from "@/lib/calendrier-store";
-import { formatLundi1Janvier } from "@/lib/util";
+import { formatLundi1Janvier, getStartOfTodayInParis } from "@/lib/util";
 
 export default function DateSelector() {
   const date = useCalendrierStore((s) => s.date);
@@ -21,21 +19,25 @@ export default function DateSelector() {
     setDate(date.plus({ days: 1 }));
   }, [date, setDate]);
 
-  const today = DateTime.now().startOf("day");
-  const isFutureDate = date > today; // C
+  const today = getStartOfTodayInParis(); // A
+  const canGoBackInTime = date > today; // C
 
   return (
-    <div className="flex items-center justify-center">
-      {isFutureDate && ( // Render LeftArrow only if the selected date is greater than today
+    <>
+      {canGoBackInTime ? (
         <div
           onClick={onClickLeft}
           className="flex grow cursor-pointer justify-end pr-5px"
         >
           <LeftArrow />
         </div>
+      ) : (
+        <div className="flex grow justify-end pr-5px" />
       )}
       <div>
-        <SousTitre1>{formatLundi1Janvier(date)}</SousTitre1>
+        <SousTitre1 className={canGoBackInTime ? "ml-5px" : ""}>
+          {formatLundi1Janvier(date)}
+        </SousTitre1>
       </div>
       <div
         onClick={onClickRight}
@@ -43,6 +45,6 @@ export default function DateSelector() {
       >
         <RightArrow />
       </div>
-    </div>
+    </>
   );
 }
