06998fe20cd6628672fa477c36e72cd632329586
Jonathan Roitgrund <jroitgrund@gmail.com>
2024-03-15 12:46:38 +0100

    Fix lint

--- src/app/film/[movieId]/film.tsx
+++ src/app/film/[movieId]/film.tsx
@@ -194,7 +194,7 @@ function DateScreenings({
     <div className="col-span-full grid grid-cols-[subgrid] border-b py-12px lg:py-16px lg:hover:bg-retro-pale-green">
       <BodyCopy>{capitalize(formatMerJJMM(safeDate(date)))}</BodyCopy>
       <div className="flex flex-col">
-        <Seances showtimes_theater={theaters} timesPerLine={2} />
+        <Seances showtimes_theater={theaters} />
       </div>
     </div>
   );
