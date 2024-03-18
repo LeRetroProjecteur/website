517dec2cae91d3b4fa0c990703c421f07210cd5f
tanouch <ugo.tanielian@outlook.fr>
2024-03-18 18:10:37 +0100

    changes

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,19 +5,28 @@ import FooterLinks from "./footer-links";
 
 export default function Footer() {
   return (
-    <div className="flex grow flex-col lg:h-94px lg:border-t lg:pt-10px">
+    <div className="flex grow flex-col lg:h-146px lg:border-t lg:pt-10px">
       <div className="flex lg:hidden">
         <FooterLinks color="gray" />
       </div>
       <div className="flex flex-col pt-20px lg:grow lg:justify-between lg:pt-0">
-        <div className="pt-20px text-center lg:pt-0 lg:text-left">
+        <div className="text-center lg:text-left">
           <FooterCopy>
-            <Link href="/admin/tous-les-films">©</Link> Le Rétro Projecteur
-            2021–2024.
+            Un problème sur le site ?<span className="hidden lg:inline"> </span>
+            <br className="lg:hidden" />{" "}
+            <a
+              href="mailto:contact@leretroprojecteur.com"
+              className="underline"
+            >
+              Signalez-le nous&nbsp;!
+            </a>
           </FooterCopy>
         </div>
-        <div className="pt-20px text-center lg:pt-0 lg:text-left">
+          <div className="pt-20px text-center lg:pt-0 lg:text-left">
           <FooterCopy>
+            <Link href="/admin/tous-les-films">©</Link> Le Rétro Projecteur
+            2021–2024
+            <br />
             Designé par{" "}
             <a
               href="https://clairemalot.com/"
