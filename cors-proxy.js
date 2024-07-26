f725f95064eea70d4ff3579840d37920219e1bde
tanouch <ugo.tanielian@outlook.fr>
2024-07-26 20:54:21 +0200

    cleaning

--- cors-proxy.js
+++ cors-proxy.js
@@ -1,11 +1,17 @@
 const cors_proxy = require("cors-anywhere");
 
-const host = "localhost";
-const port = 3000;
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 3000;
 
 cors_proxy
   .createServer({
-    originWhitelist: [], // Allow all origins
+    originWhitelist: [
+      "https://leretroprojecteur.com",
+      "https://leretroprojecteur-staging.fly.dev",
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://deploy-preview-335--leretroprojecteur.netlify.app",
+    ],
     requireHeader: ["origin", "x-requested-with"],
     removeHeaders: ["cookie", "cookie2"],
   })
