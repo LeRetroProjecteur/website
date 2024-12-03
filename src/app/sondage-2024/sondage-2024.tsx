d04369295dffbbcf498c84fef5d3277cc238c99c
tanouch <ugo.tanielian@outlook.fr>
2024-12-03 20:36:12 +0100

    removing lorem ipsum

--- src/app/sondage-2024/sondage-2024.tsx
+++ src/app/sondage-2024/sondage-2024.tsx
@@ -372,11 +372,6 @@ export default function Sondage2024({
               <SharePage rowsData={rowsData} fullName={fullName} />
             ) : (
               <>
-                {/* Instructions */}
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
                 {/* Name */}
                 <TextInputBox
                   placeholder="Votre nom/pseudo (facultatif)"
