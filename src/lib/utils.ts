559e986acff40ac3d3b9066180a438aa26586418
Jonathan Roitgrund <jroitgrund@gmail.com>
2025-01-28 23:13:27 +0100

    misc fixes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function usePrevious<T>(value: T) {
+  const [{ curr, prev }, setCurrAndPrev] = useState<{ curr: T; prev: T }>({
+    curr: value,
+    prev: value,
+  });
+
+  useEffect(() => {
+    if (curr !== value) {
+      setCurrAndPrev({
+        curr: value,
+        prev: curr,
+      });
+    }
+  }, [curr, value, setCurrAndPrev]);
+
+  return prev;
 }
