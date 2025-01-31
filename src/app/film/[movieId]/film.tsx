89c37be93e94649ab9245a25a7d1775cd0f41f37
Jonathan Roitgrund <jroitgrund@gmail.com>
2025-01-31 20:35:54 +0100

    prettier

--- src/app/film/[movieId]/film.tsx
+++ src/app/film/[movieId]/film.tsx
@@ -194,8 +194,8 @@ function MovieInformation({
                 )
               </BodyCopy>
             </div>
-            <div className="grid-cols-fillMin300 grid gap-x-8px gap-y-10px">
-              <div className="grid-cols-fillMinHalf grid gap-x-8px gap-y-10px">
+            <div className="grid grid-cols-fillMin300 gap-x-8px gap-y-10px">
+              <div className="grid grid-cols-fillMinHalf gap-x-8px gap-y-10px">
                 <Button variant="outline" asChild>
                   <Link
                     target="_blank"
@@ -213,7 +213,7 @@ function MovieInformation({
                   </Link>
                 </Button>
               </div>
-              <div className="grid-cols-fillMinHalf grid gap-x-8px gap-y-10px">
+              <div className="grid grid-cols-fillMinHalf gap-x-8px gap-y-10px">
                 {tmdbMovie.wikipediaFrUrl != null ? (
                   <Button variant="outline" asChild>
                     <Link target="_blank" href={tmdbMovie.wikipediaFrUrl}>
