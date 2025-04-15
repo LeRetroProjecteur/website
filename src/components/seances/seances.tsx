b76bc8efed45952d6a4e980bf797c37a604826eb
tanouch <ugo.tanielian@outlook.fr>
2025-04-15 23:10:56 +0200

    troubleshooting accompagnement

--- src/components/seances/seances.tsx
+++ src/components/seances/seances.tsx
@@ -106,21 +106,39 @@ export function FormatNotes({
   );
   const needsExpanding = notes.length > maxLength;
   const expandedClassName = maxLength === 0 && isExpanded ? "block" : "";
+
+  // Function to handle specific word formatting
+  const formatNotes = (text: string) => {
+    return text
+      .split(/\s+/)
+      .map((word) => (word === "Accompagnement" ? "Accompa-\n gnement" : word))
+      .join(" ");
+  };
+
   return (
     <>
       {needsExpanding ? (
         <span
-          className={`-mx-2 -my-1 cursor-pointer px-2 py-1 ${expandedClassName}`}
+          className={`
+            -mx-2 -my-1 
+            cursor-pointer 
+            px-2 
+            py-1 
+            ${expandedClassName}
+            whitespace-pre-wrap
+          `}
           onClick={toggleExpanded}
         >
           {isExpanded
-            ? notes
+            ? formatNotes(notes)
             : maxLength === 0
               ? "[...]"
-              : notes.substring(
-                  0,
-                  notes.substring(0, maxLength).lastIndexOf(" ") + 1,
-                ) + "[...]"}
+              : formatNotes(
+                  notes.substring(
+                    0,
+                    notes.substring(0, maxLength).lastIndexOf(" ") + 1,
+                  ) + "[...]",
+                )}
         </span>
       ) : (
         notes
