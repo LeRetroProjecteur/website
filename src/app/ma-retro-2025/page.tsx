3777c3e4f437e0d226bf0c7f09f680384ba3f35a
tanouch <ugo.tanielian@outlook.fr>
2025-12-20 16:01:30 +0100

    troubleshoot

--- src/app/ma-retro-2025/page.tsx
+++ src/app/ma-retro-2025/page.tsx
@@ -3,22 +3,22 @@ import { Metadata } from "next";
 import PageHeader from "@/components/layout/page-header";
 import { SousTitre1 } from "@/components/typography/typography";
 
-import MaRetro2024 from "./ma-retro-2025";
+import MaRetro2025 from "./ma-retro-2025";
 
 export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
-  title: "Ma Rétrospective 2024",
+  title: "Ma Rétrospective 2025",
   description: "Votez pour vos plus belles découvertes cinéma de l'année !",
 };
 
 export default function SubmitScreeningsPage() {
   return (
     <>
-      <PageHeader text="Ma Rétro 2024">
+      <PageHeader text="Ma Rétro 2025">
         <SousTitre1>Votez pour vos plus belles découvertes&nbsp;!</SousTitre1>
       </PageHeader>
-      <MaRetro2024 />
+      <MaRetro2025 />
     </>
   );
 }
